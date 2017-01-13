047b3912a9ec0ab91b8f694e0ceef876c29adb1b
Thom <thom.espach@sirenum.com>
2017-01-13 15:16:04 +0000

    Added further mysql configuration
    Added sample code to database controller for connecting to database.

--- controllers/dbController.php
+++ controllers/dbController.php
@@ -5,10 +5,22 @@ class DBController{
 
   private static $user='';
   private static $password='';
-  private static $db='localdb';
-  private static $host='localhost';
+  private static $db='';
+  private static $host='';
 
   public function connect(){
+
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
+        }
+
+        $this::$host = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $this::$db = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $this::$user = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $this::$password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
+
     $this->link = new mysqli($this::$host,
                           $this::$user,
                           $this::$password,
