4502106bbb411eb523ce9261f076efa5937ee39e
Thom <thom.espach@sirenum.com>
2017-01-13 15:41:07 +0000

    Updated loadUserInfo.php
    fixed $_GET request to display username and edit for snippets.

--- controllers/loadUserInfo.php
+++ controllers/loadUserInfo.php
@@ -1,33 +1,29 @@
 <?php
   require_once(__DIR__.'/../controllers/dbController.php');
   $dbController = new DBController();
+
   if(!isset($_SESSION['user_id'])){ die('You are not logged in.'); } //this is to prevent unauthorised users trying to add snippets
-  $url=$_SERVER['REQUEST_URI'];
-  $urlreq=substr($url, -1);
-  $icon;$uname;$hp;
+  if(isset($_GET['user'])){
+    $user_id = $_GET['user'];
+    $own=false;
+  } else {
+    $user_id = $_SESSION['user_id'];
+    $own=true;
+  }
+  
   $mysqli=$dbController->connect();
   $query = 'SELECT username,icon_url,homepage_url FROM users WHERE user_id=?;';
   if($stmt = $mysqli->prepare($query) or die('Query not satisfactory')){
       error_log('prepared query');
-      if ($urlreq=="p"){
-        $stmt->bind_param('s',$_SESSION['user_id']);
-        $own=true;
-      }else{
-        $stmt->bind_param('s',$urlreq);
-        $own=false;
-      }
-      error_log('binded parameters');
-      $stmt->bind_result($icon,$uname,$hp);
+      $stmt->bind_param('i',$user_id);
+      $stmt->bind_result($usename,$icon,$homepage);
       while($stmt->fetch()){
-        $icon= $result[icon_url];
-        $uname=$result[username];
-        $hp=$result[homepage_url];
         $small=substr($hp, 0, 30);
         echo '<img src="'.$icon.'">';
         echo '<p class="side-content">';
-        echo $uname;
-        echo '<a style="display:block;color:blue;" href="'.$hp.'">'.$small.'';
-        if (strlen($hp)>30){
+        echo $username;
+        echo '<a style="display:block;color:blue;" href="'.$homepage.'">'.$small.'';
+        if (strlen($homepage)>30){
           echo '...';
         }
         echo '</a>';
