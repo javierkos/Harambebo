66c0ea62922d9b5f238f352630fa2aeb001e2785
Thom <thom.espach@sirenum.com>
2017-01-13 15:25:34 +0000

    removed sample code from login.php

--- login.php
+++ login.php
@@ -12,22 +12,6 @@
 </head>
 
 <body>
-  <?php
-
-    $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-
-    if (!$link) {
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
-
-    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-    mysqli_close($link);
-?>
   <header>
 		  <h1 class="logo">Harambebo</h1>
 	</header>
