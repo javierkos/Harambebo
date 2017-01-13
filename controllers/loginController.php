3c2e29e3e277a390a37a8db2497e7adb64114e2f
Thom <thom.espach@sirenum.com>
2017-01-13 15:30:33 +0000

    removed mvc for debugging purposes

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -1,5 +1,6 @@
 <?php
-require_once(__DIR__.'/../models/user.php');
+require_once(__DIR__.'/../controllers/dbController.php');
+$dbController = new DBController();
 session_start();
 /*** check if the users is already logged in ***/
 if(isset( $_SESSION['user_id'] ))
@@ -42,27 +43,57 @@ elseif (ctype_alnum($_POST['password']) != true)
 else
 {
     /*** if we are here the data is valid and we can insert it into database ***/
-    $user = new User($_POST['username']);
-    $user->dbPopulate(); //populate the user object from database
-    $password = $_POST['password'];
+    $mysqli=$dbController->connect();
 
+    $username = mysqli_real_escape_string($mysqli, $_POST['username']); //escape post data to protect against sql injections
+    $pass = mysqli_real_escape_string($mysqli, $_POST['password']);
 
-    if($user->loginattempts > 3) {
-      die("Your account has been locked out, further password requests will not be checked. Email 'helpdesk@harambebo.com' to reset your password.");
-    } else {
-      if ($user->authenticateUser($_POST['password'])) {
-        $_SESSION['user_id']=$userid;
-        $user->resetLogins();
-        header("Location: ../home.php");
-      } else {
-        error_log('password not verified',0);
-        $user->incrementLogins();
-        session_unset();
-        session_destroy();
-        session_regenerate_id(true);
-        header("Location: ../login.php");
+    $query = "SELECT user_id,password,loginattempts FROM users WHERE username=?;";
+
+    if($stmt = $mysqli->prepare($query) or die('Query not satisfactory')){
+        $stmt->bind_param('s',$username);
+
+        /* Execute query */
+        $stmt->execute();
+
+        /* Get the result */
+        $stmt->bind_result($userid,$hashpass,$loginattempts);
+        while ($stmt->fetch()) {
+          $userid=$userid;$hashpass=$hashpass;$loginattempts=$loginattempts;
+        }
+        if($loginattempts > 3) {
+          die("Your account has been locked out, further password requests will not be checked. Email 'helpdesk@harambebo.com' to reset your password.");
+        } else {
+          if (password_verify($pass, $hashpass)) {
+            $stmt->free_result();
+            $stmt->close();
+            $_SESSION['user_id']=$userid;
+            $query= 'UPDATE users SET loginattempts=0 WHERE user_id=?;';
+            $mysqli=$dbController->connect();
+            if($stmt = $mysqli->prepare($query) or die($mysqli->error)){
+              $stmt->bind_param('i',$userid);
+              $stmt->execute();
+            }
+            header("Location: ../home.php");
+          } else {
+            error_log('password not verified',0);
+            $loginattempts+=1;
+            $stmt->free_result();
+            $stmt->close();
+            $query= 'UPDATE users SET loginattempts=? WHERE user_id=?;';
+            $mysqli=$dbController->connect();
+            if($stmt = $mysqli->prepare($query) or die($mysqli->error)){
+              $stmt->bind_param('ii',$loginattempts,$userid);
+              $stmt->execute();
+            }
+            session_unset();
+            session_destroy();
+            session_regenerate_id(true);
+            header("Location: ../login.php");
+          }
+        }
       }
-    }
+
 }
 
 ?>
--- controllers/registrationController.php
+++ controllers/registrationController.php
@@ -1,32 +1,63 @@
 <?php
-require_once(__DIR__.'/../models/user.php');
+
+require_once(__DIR__.'/../controllers/dbController.php');
 session_start();
+$dbController = new DBController();
 
 /*** check the username is the correct length ***/
-if (strlen( $_POST['username']) > 20 || strlen($_POST['username']) < 4) {
-    die('Incorrect Length for Username');
+if (strlen( $_POST['username']) > 20 || strlen($_POST['username']) < 4)
+{
+    $message = 'Incorrect Length for Username';
     header("Location: ../register.php");
 }
 /*** check the password is the correct length ***/
-elseif (strlen( $_POST['password']) > 20 || strlen($_POST['password']) < 4) {
-    die('Incorrect Length for Password');
+elseif (strlen( $_POST['password']) > 20 || strlen($_POST['password']) < 4)
+{
+    $message = 'Incorrect Length for Password';
     header("Location: ../register.php");
 }
-elseif (ctype_alnum($_POST['username']) != true) {
-    die("Username must be alpha numeric");
+/*** check the username has only alpha numeric characters ***/
+elseif (ctype_alnum($_POST['username']) != true)
+{
+    /*** if there is no match ***/
+    $message = "Username must be alpha numeric";
     header("Location: ../register.php");
 }
-elseif (ctype_alnum($_POST['password']) != true) {
-    die("Password must be alpha numeric");
+/*** check the password has only alpha numeric characters ***/
+elseif (ctype_alnum($_POST['password']) != true)
+{
+    /*** if there is no match ***/
+    $message = "Password must be alpha numeric";
     header("Location: ../register.php");
 }
-else {
+else
+{
 
-  $user = new User($_POST['username']);
-  $user->manualPopulate($_POST['password'],$_POST['icon'],$_POST['hp']);
-  $user->insert();
-  $_SESSION['user_id']=$mysqli->insert_id;
-  header("Location: ../home.php");
+  $mysqli=$dbController->connect();
 
+  $username = mysqli_real_escape_string($mysqli, $_POST['username']);
+  $pass = mysqli_real_escape_string($mysqli, $_POST['password']);
+  $icon = mysqli_real_escape_string($mysqli, $_POST['icon']);
+  $hp = mysqli_real_escape_string($mysqli, $_POST['hp']);
+
+  $options = [
+    'cost' => 11,
+    'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+  ];
+
+  $hashpass = password_hash($pass, PASSWORD_BCRYPT, $options); //Hash the password with a salt before storing it.
+
+  $query = 'INSERT INTO users VALUES(NULL,?,?,?,?,0,0)';
+
+  if($stmt = $mysqli->prepare($query) or die('Query not satisfactory')){
+
+      $stmt->bind_param('ssss',$username, $hashpass,$icon,$hp);
+
+      /* Execute query */
+      $stmt->execute();
+      $_SESSION['user_id']=$mysqli->insert_id;
+      header("Location: ../home.php");
+
+  }
 }
 ?>
