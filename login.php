6480cdc0ea7bcec6141796139b7c45e6d51a47a1
Thom <thom.espach@sirenum.com>
2017-01-13 14:47:37 +0000

    Added sample code for connecting to database
    Added some sample code to retrieve connection string so we can access
    phpmyadmin from Azure instance.

--- login.php
+++ login.php
@@ -1,18 +1,48 @@
 
-<html >
+<html>
 <head>
   <meta charset="UTF-8">
   <title>Harambebo</title>
   <meta name="viewport" content="width=device-width">
-<link href="css/login.css" rel='stylesheet' type='text/css'>
+  <link href="css/login.css" rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type='text/css' href="css/popup.css">
-
-      <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style.css">
 
 
 </head>
 
 <body>
+  <?php
+    $connectstr_dbhost = '';
+    $connectstr_dbname = '';
+    $connectstr_dbusername = '';
+    $connectstr_dbpassword = '';
+
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
+        }
+
+        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
+
+    $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+    mysqli_close($link);
+?>
   <header>
 		  <h1 class="logo">Harambebo</h1>
 	</header>
