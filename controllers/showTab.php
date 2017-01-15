3d8c0a3ff154f589d9b5b95683da4e4df14ca7aa
Thom <thom.espach@sirenum.com>
2017-01-15 18:58:54 +0000

    updated session management

--- controllers/showTab.php
+++ controllers/showTab.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  if(isset($_SESSION['user_id']) && $_COOKIE['token'] == $_SESSION['user_id']*420){ 
+  if(isset($_SESSION['user_id'])){ 
     echo '<li class="nav-item"><a href="add.php">My Snippets</a></li>';
     }
 
