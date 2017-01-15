e60545e97aebca9420163f42b9c61f2ed121022a
Thom <thom.espach@sirenum.com>
2017-01-15 18:53:26 +0000

    Incorrect logic in showTab view controller
    != should have been ==

--- controllers/showTab.php
+++ controllers/showTab.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  if(isset($_SESSION['user_id']) && $_COOKIE['token'] != $_SESSION['user_id']*420){ 
+  if(isset($_SESSION['user_id']) && $_COOKIE['token'] == $_SESSION['user_id']*420){ 
     echo '<li class="nav-item"><a href="add.php">My Snippets</a></li>';
     }
 
