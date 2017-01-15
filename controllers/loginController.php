83c093351426e3fa40b5542f65c3c60a25c3c103
Thom <thom.espach@sirenum.com>
2017-01-15 18:39:11 +0000

    Updated cookie code
    Getting and setting wasn’t working

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -40,7 +40,7 @@ else
       if ($user->authenticateUser($_POST['password'])) {
         session_regenerate_id(true);
         $_SESSION['user_id']=$user->user_id;
-        setcookie("token", $user->user_id*1337, time() + (86400 * 30), "/");
+        setcookie("token", $_SESSION['user_id']*420, time() + (86400 * 30), "/"); //generate a cookie for handling requests
         $user->resetLogins();
         header("Location: ../home.php");
       } else {
